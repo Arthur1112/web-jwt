81e09afc63210e33a75bebf0f4df818b8837f995
Arthur <arthur_2335@hotmail.com>
2022-06-07 13:59:47 -0400

    styled form and imported antD

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import "antd/dist/antd.css";
 
 function App() {
   const [token, setToken] = useState("");
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,8 +5,16 @@ export default function Login() {
     <>
       <h1>Login</h1>
       <Form name="login" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-        <Form.Item>
+        <Form.Item name="email" label="Email">
           <Input />
+        </Form.Item>
+        <Form.Item name="password" label="Password">
+          <Input.Password />
+        </Form.Item>
+        <Form.Item labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
         </Form.Item>
       </Form>
     </>
