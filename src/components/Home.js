726fb912a096f1f7814c35af65f4c72f2b9bcc59
Arthur <arthur_2335@hotmail.com>
2022-06-07 16:18:20 -0400

    added fetch to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,20 @@ import { Alert, Button } from "antd";
 export default function Home() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
+  const getPrivateStuff = () => {
+    fetch("http://localhost:5050/login")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          setError(data.error);
+          setMessage("");
+          return;
+        }
+        setMessage(data.message);
+        setError("");
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <>
       <h1>Home</h1>
