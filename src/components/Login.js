4e5ac3654189048457e0335e3f9dfd37aa254d16
Arthur <arthur_2335@hotmail.com>
2022-06-07 13:54:11 -0400

    added form to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,14 @@
+import { Form, Button, Input } from "antd";
+
 export default function Login() {
   return (
     <>
       <h1>Login</h1>
+      <Form>
+        <Form.Item>
+          <Input />
+        </Form.Item>
+      </Form>
     </>
   );
 }
