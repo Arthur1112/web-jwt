88172b385b6228636c94f18a679cea7f589b7cab
Arthur <arthur_2335@hotmail.com>
2022-06-07 14:14:07 -0400

    added handel login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,18 @@
 import { Form, Button, Input } from "antd";
 
 export default function Login() {
+  const handleLogin = ({ email, password }) => {
+    console.log(email, password);
+  };
   return (
     <>
       <h1>Login</h1>
-      <Form name="login" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+      <Form
+        name="login"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        onFinish={handleLogin}
+      >
         <Form.Item name="email" label="Email">
           <Input />
         </Form.Item>
