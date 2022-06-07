9a90323ed41659f131d82ef4f367538c3d1e2805
Arthur <arthur_2335@hotmail.com>
2022-06-07 14:37:35 -0400

    added .then .catch to fetch

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,19 +3,30 @@ import bcrypt from "bcryptjs";
 
 const salt = "$2b$10$3c22hEeSjfAnsuZ76BHS0O";
 
+// Have to use the same SALT for pw
 //const salt = bcrypt.genSaltSync(10)
 
 export default function Login() {
   const handleLogin = ({ email, password }) => {
     console.log(email, password);
     const hash = bcrypt.hashSync(password, salt);
+    console.log(hash);
     fetch("http://localhost:5050/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ email, password }),
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+          return;
+        }
+        console.log(data.token);
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <>
