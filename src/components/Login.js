65cfc3de87440c8af59437d7691c746dc8f9acfe
Arthur <arthur_2335@hotmail.com>
2022-06-07 13:55:13 -0400

    styled login

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ export default function Login() {
   return (
     <>
       <h1>Login</h1>
-      <Form>
+      <Form name="login" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
         <Form.Item>
           <Input />
         </Form.Item>
