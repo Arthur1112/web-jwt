154704e4eb2e2af63a699e94fe879df6ef483a72
Arthur <arthur_2335@hotmail.com>
2022-06-07 16:11:34 -0400

    imported useState into home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
   return (
     <>
       <h1>Home</h1>
